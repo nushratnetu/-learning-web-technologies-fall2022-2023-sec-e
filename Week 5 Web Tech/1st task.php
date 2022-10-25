c45394a317d80a4dc1bc55ac7d7d4a214395f04d
nushratnetu <114251654+nushratnetu@users.noreply.github.com>
2022-10-25 22:36:34 +0600

    Add files via upload

---
+++ Week 5 Web Tech/1st task.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <title>Company Interface</title>
+  </head>
+  <body>
+    <table
+      border="1"
+      align="center"
+      width="800px"
+      style="border-collapse: collapse; margin-top: 20px"
+    >
+      <tr>
+        <td><img src="img/company-logo-transparent-png-19.png" alt="Company Logo" style="width: 80px" /><a href="home.php" style="width: 100px" style="margin: 40px">Home</a> | <a href="login.php">Login</a> | <a href="register.php">Registration</a></td>
+     <br>
+    </tr>
+
+      <tr>
+        <td style="padding-bottom: 100px"><h2 style="margin: 30px">Welcome to xCompany</h2></td>
+      </tr>
+
+      <tr align="center">
+        <td>
+          <footer style="margin: 10px">Copyright &copy; 2017</footer>
+        </td>
+      </tr>
+
+    </table>
+
+  </body>
+</html>
+
+<?php
+
+?>
